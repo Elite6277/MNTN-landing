4d152758df2163e22a2d3f7ea5a59c4c8a1c9592
Elite6277 <elite6277@gmail.com>
2024-02-01 12:50:29 +0500

    1page-finished_v1.3

--- src/sections/Welcome.jsx
+++ src/sections/Welcome.jsx
@@ -5,14 +5,14 @@ const Welcome = () => {
     <>
       {welcomeSection.map((item) => (
         <div className="flex justify-center items-center">
-          <div key={item} className=" max-w-[950px]">
+          <div key={item} className=" max-w-[950px] max-sm:max-w-[100%]">
             <div className="flex items-center text-lg">
-              <div className="w-[72px] h-[2px]  bg-accent mr-8"></div>
-              <h2 className="text-accent tracking-[10px]  leading-none font-gilroy uppercase text-[18px]">
+              <div className="w-[72px] h-[2px]  bg-accent mr-8 max-sm:hidden"></div>
+              <h2 className="text-accent tracking-[10px]  leading-none font-gilroy uppercase text-[18px] max-sm:text-[10px]">
                 {item.title}
               </h2>
             </div>
-            <p className="mt-8 leading-none font-Chronicle text-[88px] text-primary capitalize max-lg:text-lg">
+            <p className="mt-8 leading-none font-Chronicle text-[88px] text-primary capitalize max-xl:text-[60px] max-sm:text-[26px]">
               {item.subtitle}
             </p>
             <div className="flex mt-[32px]">
